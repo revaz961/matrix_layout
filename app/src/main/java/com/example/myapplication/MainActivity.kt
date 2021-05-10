4efdb1c1b81ad53de799ff7312e217fa448b4518
revaz <revaz961@gmail.com>
2021-05-10 23:23:40 +0400

    matrix layout

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,7 +7,6 @@ import android.widget.Button
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        var btn = findViewById<Button>(R.id.btn1)
         setContentView(R.layout.constraint_matrix)
     }
 }
